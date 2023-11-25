d129bc0ce873d37d7cb96347a2a4446bb3bce98d
DomJen00 <dje7639@students.fhv.at>
2023-11-25 23:05:05 +0100

    Added test cases

--- src/model.py
+++ src/model.py
@@ -43,8 +43,7 @@ class Map:
     def get_neighbours(self, field):
         # Get a list of available neighbouring fields (N, S, W, E) of the given field
         # if they are not on the map, they are not available
-        x = field.x
-        y = field.y
+        x, y = field
         
         # All possible directions
         n = (x, y-1)
@@ -67,9 +66,9 @@ class Map:
         # Static method to create a Map object from a multiline string
 
         # map of all possible elevations
-        elevation_mapping = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9,
-                    'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18,
-                    's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26
+        elevation_mapping = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8,
+                    'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17,
+                    's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25
         }
 
         # Create list of all lines
@@ -109,9 +108,16 @@ class Path:
             self.fields.pop()
 
 
-multilinestring = """\
+if __name__ == '__main__':
+
+    multilinestring = """\
 abc
 def
 ghi"""
 
-print(multilinestring)
+    print(multilinestring)
+    
+    map = Map()
+    world = map.from_string(multilinestring)
+    neigh = world.get_neighbours((1, 1))
+    print(neigh)
--- tests/test_model.py
+++ tests/test_model.py
@@ -47,3 +47,80 @@ b"""
     assert world.get_field(0, 0) == Field(0, 0, 0)
     # AND the map should contain a field with elevation 1 (=b) at (x=0, y=1)
     assert world.get_field(0, 1) == Field(0, 1, 1)
+    
+
+def test_create_a_multiline_map_from_string():
+    # GIVEN a multiline string
+    multilinestring = """\
+abc
+def
+ghi"""
+
+    # WHEN creating a map from the string
+    world = Map.from_string(multilinestring)
+
+    #THEN the map should contain a field with elevation 0 (=a) at (x=0, y=0)
+    assert world.get_field(0, 0) == Field(0, 0, 0)
+    #THEN the map should contain a field with elevation 1 (=b) at (x=1, y=0)
+    assert world.get_field(1, 0) == Field(1, 0, 1)
+    #THEN the map should contain a field with elevation 2 (=c) at (x=2, y=0)
+    assert world.get_field(2, 0) == Field(2, 0, 2)
+    #THEN the map should contain a field with elevation 3 (=d) at (x=0, y=1)
+    assert world.get_field(0, 1) == Field(0, 1, 3)
+    #THEN the map should contain a field with elevation 4 (=e) at (x=1, y=1)
+    assert world.get_field(1, 1) == Field(1, 1, 4)
+    #THEN the map should contain a field with elevation 5 (=f) at (x=2, y=1)
+    assert world.get_field(2, 1) == Field(2, 1, 5)
+    #THEN the map should contain a field with elevation 6 (=g) at (x=0, y=2)
+    assert world.get_field(0, 2) == Field(0, 2, 6)
+    #THEN the map should contain a field with elevation 7 (=h) at (x=1, y=2)
+    assert world.get_field(1, 2) == Field(1, 2, 7)
+    #THEN the map should contain a field with elevation 8 (=i) at (x=2, y=2)
+    assert world.get_field(2, 2) == Field(2, 2, 8)
+
+def test_neighbour_fields_of_corner_elements():
+    # GIVEN a multiline string
+    multilinestring = """\
+abc
+def
+ghi"""
+    
+    # WHEN creating a map from the string 
+    map = Map()
+    world = map.from_string(multilinestring)
+
+    # THEN the neighbours from a should be b (x = 1, y = 0, elevation = 1) and d (x = 0, y = 1, elevation = 3)
+    expected_neighbours = [Field(0, 1, 3), Field(1, 0, 1)]    
+    neighbours = world.get_neighbours((0, 0))
+    assert expected_neighbours == neighbours
+
+    # THEN the neighbours from c should be b (x = 1, y = 0, elevation = 1) and f (x = 2, y = 1, elevation = 5)   
+    expected_neighbours = [Field(2, 1, 5), Field(1, 0, 1)]
+    neighbours = world.get_neighbours((2, 0))
+    assert expected_neighbours == neighbours
+
+    # THEN the neighbours from g should be d (x = 0, y = 1, elevation = 3) and h (x = 1, y = 2, elevation = 7)   
+    expected_neighbours = [Field(0, 1, 3), Field(1, 2, 7)]
+    neighbours = world.get_neighbours((0, 2))
+    assert expected_neighbours == neighbours
+
+    # THEN the neighbours from i should be h (x = 1, y = 2, elevation = 7) and f (x = 2, y = 1, elevation = 5)   
+    expected_neighbours = [Field(2, 1, 5), Field(1, 2, 7)]
+    neighbours = world.get_neighbours((2, 2))
+    assert expected_neighbours == neighbours
+
+def test_neighbour_fields_of_mid_element():
+    # GIVEN a multiline string
+    multilinestring = """\
+abc
+def
+ghi"""
+    
+    # WHEN creating a map from the string 
+    map = Map()
+    world = map.from_string(multilinestring)
+
+    # THEN the neighbours from e should be b (x = 1, y = 0, elevation = 1), d (x = 0, y = 1, elevation = 3), f (x = 2, y = 1, elevation = 5), h (x = 1, y = 2, elevation = 7)
+    expected_neighbours = [Field(1, 0, 1), Field(1, 2, 7), Field(0, 1, 3), Field(2, 1, 5)]    
+    neighbours = world.get_neighbours((1, 1))
+    assert expected_neighbours == neighbours
